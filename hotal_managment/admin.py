6651d350ab36704d1afb53e2eb5faaa729699b47
rahuugitit <rk8449022182@gmail.com>
2024-06-27 20:04:26 +0530

    add and update the file

--- hotal_managment/admin.py
+++ hotal_managment/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from hotal_managment.models import Room, Customer,Booking
 # Register your models here.
+
+@admin.register(Room)
+class roomAdmin(admin.ModelAdmin):
+    list_display=["id","number","room_type","price","is_available"]
+
+    
+
+@admin.register(Customer)
+class customerAdmin(admin.ModelAdmin):
+    list_display=["id","first_name","last_name","email","phone"]
+
+    
+
+
+@admin.register(Booking)
+class bookingAdmin(admin.ModelAdmin):
+    list_display=["id","room","customer","check_in","check_out"]
