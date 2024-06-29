aeaccbe806da1cbaad25f42c32a89ba84839d9a7
rahuugitit <rk8449022182@gmail.com>
2024-06-29 12:02:32 +0530

    add the update the api

--- hotal_managment/urls.py
+++ hotal_managment/urls.py
@@ -3,17 +3,18 @@ from rest_framework.routers import DefaultRouter
 from hotal_managment import views
 router=DefaultRouter()
 router.register('roomapi', views.RoomListCreate, basename="roomapi")
-router.register('bookapi', views.RoomListCreate, basename="bookapi")
-router.register('customerapi', views.RoomListCreate, basename="customerapi")
-
-# from .views import RoomListCreate, CustomerListCreate, BookingListCreate, BookingDetail, check_availability
+router.register('bookapi', views.BookingListCreate, basename="bookapi")
+router.register('customerapi', views.CustomerListCreate, basename="customerapi")
+# 
+from .views import RoomListCreate, CustomerListCreate, BookingListCreate, BookingDetail, check_availability
 
 urlpatterns = [
     # path('rooms/', RoomListCreate.as_view(), name='room-list-create'),
     # path('customers/', CustomerListCreate.as_view(), name='customer-list-create'),
     # path('bookings/', BookingListCreate.as_view(), name='booking-list-create'),
-    # path('bookings/<int:pk>/', BookingDetail.as_view(), name='booking-detail'),
-    # path('availability/', check_availability, name='check-availability'),
+    path('bookings/<int:pk>/', BookingDetail.as_view(), name='booking-detail'),
+    path('availability/', check_availability, name='check-availability'),
 
     path("", include(router.urls))
+    # path('availability/', check_availability, name='check-availability'),
 ]
--- hotal_managment/views.py
+++ hotal_managment/views.py
@@ -18,26 +18,26 @@ class BookingListCreate(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
-    # def create(self, request, *args, **kwargs):
-    #     data = request.data
-    #     room = Room.objects.get(id=data['room'])
-    #     bookings = Booking.objects.filter(room=room, check_out__gte=data['check_in'], check_in__lte=data['check_out'])
-    #     if bookings.exists():
-    #         return Response({"error": "Room is not available for the selected dates"}, status=status.HTTP_400_BAD_REQUEST)
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        room = Room.objects.get(id=data['room'])
+        bookings = Booking.objects.filter(room=room, check_out__gte=data['check_in'], check_in__lte=data['check_out'])
+        if bookings.exists():
+            return Response({"error": "Room is not available for the selected dates"}, status=status.HTTP_400_BAD_REQUEST)
         
-    #     serializer = self.get_serializer(data=data)
-    #     serializer.is_valid(raise_exception=True)
-    #     self.perform_create(serializer)
-    #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-# class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Booking.objects.all()
-#     serializer_class = BookingSerializer
+class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 
-# def check_availability(request):
-#     check_in = request.GET.get('check_in')
-#     check_out = request.GET.get('check_out')
-#     available_rooms = Room.objects.filter(is_available=True, booking__check_out__lt=date.fromisoformat(check_in)).union(Room.objects.filter(is_available=True,booking__check_in__gt=date.fromisoformat(check_out))
-#     )
-#     serializer = RoomSerializer(available_rooms, many=True)
-#     return JsonResponse(serializer.data, safe=False)
+def check_availability(request):
+    check_in = request.GET.get('check_in')
+    check_out = request.GET.get('check_out')
+    available_rooms = Room.objects.filter(is_available=True, booking__check_out__lt=date.fromisoformat(check_in)).union(Room.objects.filter(is_available=True,booking__check_in__gt=date.fromisoformat(check_out))
+    )
+    serializer = RoomSerializer(available_rooms, many=True)
+    return JsonResponse(serializer.data, safe=False)
